185c961cc13abe98b772a761d66ea990fca0c158
dh34ma <100998077+dh34ma@users.noreply.github.com>
2022-06-17 21:28:53 +0100

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,15 +1,20 @@
-P = '\x1b[1;97m'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
-try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nBAD INTERNET CONNECTION\n'%(M))
-	exit()
-
+import os
+import platform
+import webbrowser
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/KAR-DATA')
+except:pass
+try:os.system('mkdir /sdcard/KAR-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/KAR-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/KAR-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
 if __name__ == "__main__":
-	os.system("git pull")
-	if "Nigeria" == fc:
-		__import__("crack").login()
-	else:
-		__import__("crack").login()
+        try:
+                __import__("crack").login()
+        except Exception as e:
+                exit(str(e))
