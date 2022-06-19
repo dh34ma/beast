85478de4f78c5efde3200e8613ecf8aad7be99ac
dh34ma <100998077+dh34ma@users.noreply.github.com>
2022-06-19 08:59:39 +0100

    Update beast.py

--- beast.py
+++ beast.py
@@ -3,13 +3,13 @@ import platform
 import webbrowser
 os.system('termux-setup-storage')
 os.system('git pull')
-try:os.system('mkdir /sdcard/KAR-DATA')
+try:os.system('mkdir /sdcard/KHAN-DATA')
 except:pass
-try:os.system('mkdir /sdcard/KAR-DATA/OK')
+try:os.system('mkdir /sdcard/KHAN-DATA/OK')
 except:pass
-try:os.system('mkdir /sdcard/KAR-DATA/CP')
+try:os.system('mkdir /sdcard/KHAN-DATA/CP')
 except:pass
-try:os.system('mkdir /sdcard/KAR-DATA/TAP-A2F')
+try:os.system('mkdir /sdcard/KHAN-DATA/TAP-A2F')
 except:pass
 try:os.system('touch .prox.txt')
 except:pass
