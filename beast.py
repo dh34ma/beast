c0bc67c8545ef85bd5a5624832fbc57d8626939a
dh34ma <100998077+dh34ma@users.noreply.github.com>
2022-06-19 08:57:02 +0100

    Create beast.py

---
+++ beast.py
@@ -0,0 +1,20 @@
+import os
+import platform
+import webbrowser
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/KAR-DATA')
+except:pass
+try:os.system('mkdir /sdcard/KAR-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/KAR-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/KAR-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
+if __name__ == "__main__":
+        try:
+                __import__("beast").login()
+        except Exception as e:
+                exit(str(e))
